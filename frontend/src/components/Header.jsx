904115aaa0b26defeb61e75d807aeffdfb7d6c11
Maksym_Kos <maxkos18@gmail.com>
2024-01-16 15:15:46 +0200

    Redux setup with CartPage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
-import logo from '../assets/logo.png';
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const productCounter = cartItems.reduce((a, c) => a + Number(c.qty), 0);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -20,6 +24,11 @@ export const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {productCounter}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,19 +6,22 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './strore.js';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
+import CartPage from './pages/CartPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} >
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductDetailsPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Route>
   )
 )
@@ -26,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
