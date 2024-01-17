5df9863b2aacb97221c9ab33d87b27046b4c65bf
Maksym_Kos <maxkos18@gmail.com>
2024-01-17 10:57:43 +0200

    backendAuth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
@@ -10,14 +10,24 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+
+console.log(process.env)
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
